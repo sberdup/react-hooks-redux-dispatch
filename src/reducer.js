3bf577f7eda1a4286a8dba10e9cde92b197c07af
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-27 18:56:32 -0600

    Completed assignment

--- src/reducer.js
+++ src/reducer.js
@@ -10,4 +10,11 @@ function changeState(state, action) {
 let state = { count: 0 };
 let action = { type: "counter/increment" };
 
-changeState(state, action);
+function dispatch(action) {
+  state = changeState(state, action)
+  render()
+}
+function render() {
+  document.body.textContent = state.count;
+}
+render()
